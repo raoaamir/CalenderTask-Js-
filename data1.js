3adf728d85b47d5a106dad9189714a5eed1e923b
raoaamir <muhammad.amir@gigalabs.co>
2022-11-30 19:47:52 +0500

    Updated

---
+++ data1.js
@@ -0,0 +1,73 @@
+const arr=[
+    {
+          Ename: "MEETING WITH CLIENT",
+          loc: "LHR",
+          sTime: 9,
+          eTime: 10,
+    },
+        
+     {
+          Ename: "MEETING WITH CLIENT",
+          loc: "LHR",
+          sTime: 11,
+          eTime: 12.5,
+    },
+        
+     {
+          Ename: "MEETING WITH CLIENT",
+          loc: "MULTAN",
+          sTime: 13,
+          eTime: 14.5,
+        },
+        
+        {
+          Ename: "MEETING WITH CLIENT",
+          loc: "FAISALABAD",
+          sTime: 14,
+          eTime: 15,
+        },
+        
+        {
+          Ename: "MEETING WITH CLIENT",
+          loc: "ISLAMABAD",
+          sTime: 17,
+          eTime: 20,
+        },
+        
+       {
+          Ename: "MEETING WITH CLIENT",
+          loc: "LHR",
+          sTime: 17.5,
+          eTime: 19,
+        },
+        {
+          Ename: "MEETING WITH CLIENT",
+          loc: "LHR",
+          sTime: 18,
+          eTime: 20,
+        },
+    //     {
+    //     Ename: "MEETING WITH CLIENT",
+    //     loc: "LHR",
+    //     sTime: 9,
+    //     eTime: 11.5,
+    //   }
+]
+
+const daily =[
+    {
+    name:"Sample location",
+    loc :"Lahore"
+    },
+    {
+        name:"Sample location",
+        loc :"Lahore"
+    },
+    {
+        name:"Sample location",
+        loc :"Lahore"
+    }
+
+]
+
+export{arr,daily}
--- index.js
+++ index.js
@@ -1,59 +1,35 @@
+import {arr, daily} from "./data1.js"
 var p = 0
 var count;
-var eventArray = [];
 var starttime2 = 0;
 var format = 0;
 var timeform = "";
 
-var event1 = {
-  Ename: "MEETING WITH CLIENT",
-  loc: "LHR",
-  sTime: 9,
-  eTime: 10,
-}
+function setAllDay(){
 
-var event2 = {
-  Ename: "MEETING WITH CLIENT",
-  loc: "LHR",
-  sTime: 11,
-  eTime: 12.5,
-}
+  daily.forEach((element)=>{
+    let allday = document.createElement("div");
+    let x = document.createElement("p")
+    let c = document.createElement("span")
+    let y = document.createElement("p")
+    x.innerHTML = "ALL DAY-";
+    c.innerHTML = element.name; 
+    y.innerHTML = element.loc;
 
-var event3 = {
-  Ename: "MEETING WITH CLIENT",
-  loc: "MULTAN",
-  sTime: 13,
-  eTime: 14.5,
-}
 
-var event4 = {
-  Ename: "MEETING WITH CLIENT",
-  loc: "FAISALABAD",
-  sTime: 14,
-  eTime: 15,
-}
+    allday.appendChild(x);
+    allday.appendChild(c);
+    allday.appendChild(y);
 
-var event5 = {
-  Ename: "MEETING WITH CLIENT",
-  loc: "ISLAMABAD",
-  sTime: 17,
-  eTime: 20,
-}
+    allday.setAttribute("class","inner")
+    c.style.color = "green"
 
-var event6 = {
-  Ename: "MEETING WITH CLIENT",
-  loc: "LHR",
-  sTime: 17.5,
-  eTime: 19,
-}
-var event7 = {
-  Ename: "MEETING WITH CLIENT",
-  loc: "LHR",
-  sTime: 18,
-  eTime: 20,
-}
-eventArray = [event1, event2, event3, event4, event5, event6, event7]
+    let d = document.getElementById("outer");
+    d.appendChild(allday);
 
+  });
+}
+setAllDay();
 function createEvent(eventName, loc, time, endtime) {
 
   settime(time);
@@ -102,22 +78,19 @@ function eventOverlap(evt1, evt2) {
 
 var elementArray = [];
 
-eventArray.forEach(element => {
+arr.forEach(element => {
   var m = createEvent(element.Ename, element.loc, element.sTime, element.eTime)
-  console.log(m, "iii")
+  // console.log(m, "iii")
   elementArray.push(m);
 
 
 });
 
-
-
-
-for (i = 0; i < elementArray.length; i++) {
-  for (j = i + 1; j < elementArray.length; j++) {
+for (let i = 0; i < elementArray.length; i++) {
+  for (let j = i + 1; j < elementArray.length; j++) {
     if (eventOverlap(elementArray[i], elementArray[j])) {
       var s = elementArray[i].parentElement;
-      var height = ((s.id - eventArray[j].sTime) * -1.9 * 2).toString() + "rem";
+      var height = ((s.id - arr[j].sTime) * -1.9 * 2).toString() + "rem";
       s.appendChild(elementArray[j]);
       elementArray[j].style.marginTop = height;
 
@@ -143,6 +116,9 @@ function settime(time) {
     starttime2 = starttime2.toString() + ":00" + timeform;
   }
 }
+
+
+
 
 
 
